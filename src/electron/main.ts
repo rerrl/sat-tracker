aecb1d1c8f897f0c8d6f7d502c580098d180eee2
rerrl <andrew@satoshi.money>
2024-11-23 20:04:20 -0800

    dx improvements

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,13 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
-
-type test = string;
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
